115ed8c813d756354b88e28f3136bf83a67d17df
darkasfuck <auriarad@gmail.com>
2025-02-04 01:54:28 +0200

    more small fixes

--- src/app/api/analytics/route.js
+++ src/app/api/analytics/route.js
@@ -1,15 +1,38 @@
 import { NextResponse } from 'next/server';
 import { BetaAnalyticsDataClient } from '@google-analytics/data';
+import { getAdminSession } from '@/lib/session';
+import Admin from '@/models/admin';
+import connectToDatabase from '@/lib/mongodb';
 
-const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
+// Decode Base64 to JSON
+const serviceAccountJSON = Buffer.from(
+    process.env.GOOGLE_SERVICE_ACCOUNT,
+    'base64'
+).toString('utf-8');
+
+const credentials = JSON.parse(serviceAccountJSON);
 
 const analyticsClient = new BetaAnalyticsDataClient({
     credentials,
     projectId: credentials.project_id,
 });
 
-export async function GET() {
+export async function GET(request) {
     try {
+        //check admin
+        await connectToDatabase();
+        const session = await getAdminSession(request.cookies.get('adminSession')?.value);
+        if (!session?.adminId) {
+            console.log("here")
+            return Response.json({ success: false, error: "נו באמת לא חשבת שזה יהיה כזה קל" }, { status: 401 });
+        }
+        const admin = await Admin.findById(session.adminId);
+        if (!admin) {
+            console.log("here2")
+            return Response.json({ success: false, error: "נו באמת לא חשבת שזה יהיה כזה קל" }, { status: 401 });
+        }
+        console.log("here3")
+
         const [response] = await analyticsClient.runReport({
             property: `properties/${process.env.GA4_PROPERTY_ID}`,
             dateRanges: [{ startDate: '7daysAgo', endDate: 'today' }],
