be8952a54904feae779b84cc245c374322eccd0d
darkasfuck <auriarad@gmail.com>
2025-02-04 16:41:29 +0200

    random force dynamic

--- src/app/jokes/random/page.jsx
+++ src/app/jokes/random/page.jsx
@@ -2,6 +2,8 @@ import Joke from "@/models/Joke";
 import { redirect } from 'next/navigation'
 import connectToDatabase from "@/lib/mongodb";
 
+export const dynamic = 'force-dynamic'
+
 export default async function RandomJoke() {
     await connectToDatabase();
     const count = await Joke.countDocuments({ approved: true });
